b2ae42d995f93e69544f99c8d82d32b22b195595
Sachini0797 <sach.790@gmail.com>
2024-03-09 14:34:00 +0530

    Project init

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,26 @@
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [DatePipe, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  todayRealtimeDate: number = Date.now();
+
+  constructor(private router: Router,) {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.todayRealtimeDate = Date.now();
+    }, 1);
+  }
+  login(): void {
+    console.log("hello")
+    this.router.navigate(['/login']);
+  }
+}
